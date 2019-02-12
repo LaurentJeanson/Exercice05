d8df1837f69d7a994cc57e47ea31051ba8f5e7cc
Laurent Jeanson <43211608+devjeu@users.noreply.github.com>
2019-02-12 11:48:05 -0500

    Arrangement des bugs et animation de lettres

--- module_js/animLettre.js
+++ module_js/animLettre.js
@@ -7,16 +7,16 @@ export class AnimLettre {
 
     /**
      * Classe permettant de créer et d'animer une introduction
-     * @param {object} o - contient l'ensemble des mots d'intro
+     * @param {string} lesLettres - contient l'ensemble des mots d'intro
      * @param {DOMElement} elementParent - Conteneur de l'animation
      * @param {function} fonction - l'adresse de la fonction à exécuter après l'animation
          
      }}
      */
 
-    constructor(o, elementParent, fonction) {
+    constructor(lesLettres, elementParent, fonction) {
         //Récupérer les valeurs passées en paramètre			
-        this.lesLettres = o.lesLettres;
+        this.lesLettres = lesLettres;
         this.elmParent = elementParent;
         this.anim_lettres(this.lesLettres);
         this.fonction = fonction;
@@ -32,14 +32,15 @@ export class AnimLettre {
             'section',
             '',
             'mot')
-
+            let i = 0;
             for (let uneLettre of lesLettres) {
-                uneLettre.classList.add("animationLettre");
-                uneLettre.style.color = tabCouleur[i++]
+                let elmLettre = this.creerElement(elmConteneur, 'div', uneLettre, "mot")
+                elmLettre.style.animationDelay = (i * 0.5) + "s";
+                i++;
             }
         /* On garde une référence sur la fonction terminerIntro */
         let refTerminerIntro = this.terminerIntro.bind(this)
-        elmBouton.addEventListener('mousedown', this.terminerIntro.bind(this))
+        //lmBouton.addEventListener('mousedown', this.terminerIntro.bind(this))
     }
 
     creerElement(elmParent, balise, contenu, classCSS) {
